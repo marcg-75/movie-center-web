6819b5855fce0a35b8f15722c3bfcd9fb752cb49
Marc Giron <marcg.mg@gmail.com>
2023-11-15 21:04:35 +0100

    Migrate to React 18 done

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
+import React, { ReactNode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import configureStore from './store';
 import App from './components/App';
 import MovieList from './components/movie-list/MovieList';
@@ -9,12 +10,12 @@ import Movie from './components/movie/Movie';
 
 // TODO: Currently not in use. Possibly a move to Next.js is required.
 
-export default function Index() {
+// export default function Index(): ReactNode {
     const routes = [
-        { path: '/movies', component: MovieList },
-        { path: '/movie/:id', component: Movie },
+        {path: '/movies', component: MovieList},
+        {path: '/movie/:id', component: Movie},
         //{ path: '', redirectTo: 'movie', pathMatch: 'full' }
-        { path: '', component: MovieList }
+        {path: '', component: MovieList}
     ];
 
     const {store, persistor} = configureStore();
@@ -24,63 +25,32 @@ export default function Index() {
 //store.dispatch(loadAllRbcs());
 //store.dispatch(loadUserInfo());
 
-    return (
-        <>
-            <span>Pip</span>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <Router>
-                    <App>
-                        <Switch>
-                            {routes.map((route, i) => (
-                                <Route path={route.path} component={route.component} key={i} />
-                            ))}
-                        </Switch>
-                    </App>
-                </Router>
-            </PersistGate>
-        </Provider>
-        </>
+    //injectGlobal`${theme.skins.Global(theme, process.env.NODE_ENV)}`; // eslint-disable-line no-unused-expressions
+
+    const root = ReactDOM.createRoot(
+        document.getElementById('root') as HTMLElement
     );
-}
 
-// const render = () => {
-//     ReactDOM.render(
-//         <Provider store={store}>
-//             <PersistGate loading={null} persistor={persistor}>
-//                 <Router>
-//                     <App>
-//                         <Switch>
-//                             {routes.map((route, i) => (
-//                                 <Route path={route.path} component={route.component} key={i} />
-//                             ))}
-//                         </Switch>
-//                     </App>
-//                 </Router>
-//             </PersistGate>
-//         </Provider>,
-//         document.getElementById('root')
-//     );
-// };
-
-/*
-const render = () => {
-    ReactDOM.render(
-        <ThemeProvider theme={theme}>
+    root.render(
+        <React.StrictMode>
+            {/*<ThemeProvider theme={theme}>*/}
             <Provider store={store}>
-                <Router>
-                    <Route path="/" component={App} />
-                </Router>
-            </Provider>
-        </ThemeProvider>,
-        document.getElementById('root')
+                <PersistGate loading={null} persistor={persistor}>
+                    <Router>
+                        <App>
+                            <Switch>
+                                {routes.map((route, i) => (
+                                    <Route path={route.path} component={route.component} key={i} />
+                                ))}
+                            </Switch>
+                        </App>
+                    </Router>
+                </PersistGate>
+            </Provider>,
+            {/*</ThemeProvider>*/}
+        </React.StrictMode>
     );
-};
-*/
-
-//injectGlobal`${theme.skins.Global(theme, process.env.NODE_ENV)}`; // eslint-disable-line no-unused-expressions
-
-// render();
+// }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
