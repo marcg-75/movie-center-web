b745194cd96fa6b9d0c52a4759cc38b742af341a
Marc Giron <marcg.mg@gmail.com>
2024-01-03 16:02:06 +0100

    fix: test

--- libs/shared/data-access-api-client/src/api-client.test.ts
+++ libs/shared/data-access-api-client/src/api-client.test.ts
@@ -1,19 +1,10 @@
 import { buildPath } from './fetch-utils';
 
 describe('buildPath', () => {
-  it('adds noauth to the path when the user is not authenticated', () => {
+  it('returns the path as is', () => {
     const path = 'mathem-service/some/path';
-    const authenticated = false;
-    const expected = 'mathem-service/noauth/some/path';
-
-    expect(buildPath(path, authenticated)).toBe(expected);
-  });
-
-  it('returns the path as is when the user is authenticated', () => {
-    const path = 'mathem-service/some/path';
-    const authenticated = true;
     const expected = 'mathem-service/some/path';
 
-    expect(buildPath(path, authenticated)).toBe(expected);
+    expect(buildPath(path)).toBe(expected);
   });
 });
