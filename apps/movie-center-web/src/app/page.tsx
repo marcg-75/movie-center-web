024df2fb639ab7bd683da8f3c7296869cd79fd5f
Marc Giron <marcg.mg@gmail.com>
2024-01-03 15:59:28 +0100

    fix: pre-build

--- apps/movie-center-web/src/app/page.tsx
+++ apps/movie-center-web/src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Index() {
   const router = useRouter();
 
-  // Redirect to movie list page.
-  router.push('/movies');
+  // Wrapping the automatic redirect inside an effect hook. Otherwise the pre-build check will fail.
+  useEffect(() => {
+    // Redirect to movie list page.
+    router.push('/movies');
+  }, []);
 
   return <></>;
 }
