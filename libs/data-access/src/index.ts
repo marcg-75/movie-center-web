f85bf13ac47207513d1809cedff78156e236dcaf
marcg-75 <marcg-75@users.noreply.github.com>
2024-01-09 16:10:38 +0100

    mutate hooks for movie details crud (#10)

--- libs/data-access/src/index.ts
+++ libs/data-access/src/index.ts
@@ -5,5 +5,12 @@ export {
   useRoles,
   useLanguages,
 } from './base-data';
-export { useMovieList, useMovieDetails, useMovieFilter } from './movie';
+export {
+  useMovieList,
+  useMovieDetails,
+  useMovieFilter,
+  useCreateMovieMutation,
+  useUpdateMovieMutation,
+  useDeleteMovieMutation,
+} from './movie';
 export { useActors, useCrew, useAllPersons } from './person';
